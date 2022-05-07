def18de49a015448f2c3f8bf3f87116d53ed49cc
Ariel Leonardo Fideleff <68936651+ariloc@users.noreply.github.com>
2022-05-06 22:20:52 -0300

    Modified the program to infinitely ask for two numbers

--- sum_of_two_numbers.hs
+++ sum_of_two_numbers.hs
@@ -1,6 +1,10 @@
-main = do
+-- Now we keep asking the user infinitely for two numbers to add!
+inf_input = do
   putStr "Enter the first number: "
   a <- readLn
   putStr "Enter the second number: "
   b <- readLn
-  putStrLn $ "The result is " ++ (show (a + b)) ++ "!"
+  putStrLn $ "The result is " ++ (show (a + b)) ++ "!\n"
+  inf_input
+
+main = inf_input
